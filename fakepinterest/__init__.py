57715deda8deb4f7d29a32c67833d333096f4354
Clebio Teodoro Alves <106354414+Clebiotalves@users.noreply.github.com>
2023-08-13 16:02:48 -0300

    mudanças no banco de dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "a39cbd6a7b41dd4509eaed58fb3a1608"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
